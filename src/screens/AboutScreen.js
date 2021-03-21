f280ea32a0ce152414660c5e6b65f732000f523a
Rhys <lomirus@qq.com>
2021-03-21 19:42:21 +0800

    feat: simple AboutScreen

--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -4,17 +4,41 @@ import { View, Text } from 'react-native';
 export default class AboutScreen extends React.Component {
     constructor(props) {
         super(props)
+
     }
     render() {
         return (
             <View style={{
                 alignItems: 'center',
-                justifyContent: 'center',
+                justifyContent: 'space-around',
                 flex: 1
             }}>
-                <Text style={{
-                    fontSize: 32,
-                }}>About</Text>
+
+                <View style={{
+                    justifyContent: 'center',
+                    flex: 1,
+                }}>
+                    <Text style={{
+                        fontSize: 32,
+                    }}>Counter Trainer</Text>
+                </View>
+                <View style={{
+                    marginBottom: 16,
+                    alignItems: 'center',
+                }}>
+                    <Text style={{
+                        color: '#666'
+                    }}>Author: Lomirus</Text>
+                    <Text style={{
+                        color: '#666'
+                    }}>Github: /lomirus/counter-trainer</Text>
+                    <Text style={{
+                        color: '#666'
+                    }}>License: MIT</Text>
+                    <Text style={{
+                        color: '#666'
+                    }}>Version: 0.4.0</Text>
+                </View>
             </View>
         )
     }
