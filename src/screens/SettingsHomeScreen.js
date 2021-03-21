f6e303086ab013e7ed469a922c5b9a587d865181
Rhys <lomirus@qq.com>
2021-03-21 19:17:49 +0800

    refactor: refactor SettingsScreen to StackNavigator

---
+++ src/screens/SettingsHomeScreen.js
@@ -0,0 +1,122 @@
+import React from 'react';
+import { View, Text, Switch, Pressable } from 'react-native';
+import { store } from '../store'
+
+import SettingPicker from '../components/SettingPicker';
+
+export default class SettingsHomeScreen extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            displayLanguage: "English",
+            learnLanguage: "日本語",
+            autoSpeak: false,
+            autoNext: false,
+            nightMode: false,
+        }
+        this.unsubscribe = store.subscribe(() => {
+            this.setState(store.getState().settings)
+        })
+    }
+    componentWillUnmount() {
+        this.setState = () => {}
+        this.unsubscribe()
+    }
+    render() {
+        return (
+            <View
+                style={{
+                    flex: 1,
+                    alignItems: "center",
+                    marginTop: 32
+                }}>
+                <SettingPicker
+                    title="Display Language"
+                    selectedValue={this.state.displayLanguage}
+                    onValueChange={value => store.dispatch({
+                        type: "CHANGE_DISPLAY_LANGUAGE",
+                        payload: value
+                    })}
+                    items={["English"]}
+                />
+                <SettingPicker
+                    title="Learn Language"
+                    selectedValue={this.state.learnLanguage}
+                    onValueChange={value => store.dispatch({
+                        type: "CHANGE_LEARN_LANGUAGE",
+                        payload: value
+                    })}
+                    items={["日本語"]}
+                />
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width: "85%",
+                        height: 48,
+                        alignItems: "center",
+                    }}>
+                    <Text style={{
+                        fontSize: 18
+                    }}>Night Mode</Text>
+                    <Switch
+                        value={this.state.nightMode}
+                        onValueChange={() => store.dispatch({ type: "CHANGE_NIGHT_MODE" })} />
+                </View>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width: "85%",
+                        height: 48,
+                        alignItems: "center",
+                    }}>
+                    <Text style={{
+                        fontSize: 18
+                    }}>Auto Speak</Text>
+                    <Switch
+                        value={this.state.autoSpeak}
+                        onValueChange={() => store.dispatch({ type: "CHANGE_AUTO_SPEAK" })} />
+                </View>
+                <View
+                    style={{
+                        flexDirection: "row",
+                        justifyContent: "space-between",
+                        width: "85%",
+                        height: 48,
+                        alignItems: "center",
+                    }}>
+                    <Text style={{
+                        fontSize: 18
+                    }}>Auto Next</Text>
+                    <Switch
+                        value={this.state.autoNext}
+                        onValueChange={() => store.dispatch({ type: "CHANGE_AUTO_NEXT" })} />
+                </View>
+                <Pressable
+                    android_ripple={{
+                        color: "rgba(0,0,0,0.2)",
+                        borderless: false,
+                    }}
+                    style={{
+                        height: 48,
+                        width: "100%",
+                        flexDirection: "row",
+                        justifyContent: "center"
+                    }}>
+                    <View style={{
+                        height: 48,
+                        width: "85%",
+                        flexDirection: "row",
+                        alignItems: "center",
+                    }}>
+                        <Text
+                            style={{
+                                fontSize: 18
+                            }}>About</Text>
+                    </View>
+                </Pressable>
+            </View>
+        )
+    }
+}
--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,124 +1,22 @@
 import React from 'react';
-import { View, Text, Switch, Pressable } from 'react-native';
-import { store } from '../store'
+import { createStackNavigator } from '@react-navigation/stack'
+import SettingsHomeScreen from './SettingsHomeScreen';
 
-import SettingPicker from '../components/SettingPicker';
+const Stack = createStackNavigator()
 
-class SettingsScreen extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            displayLanguage: "English",
-            learnLanguage: "日本語",
-            autoSpeak: false,
-            autoNext: false,
-            nightMode: false,
-        }
-        this.unsubscribe = store.subscribe(() => {
-            this.setState(store.getState().settings)
-        })
-    }
-    componentWillUnmount() {
-        this.setState = () => {}
-        this.unsubscribe()
-    }
-    render() {
-        return (
-            <View
-                style={{
-                    flex: 1,
-                    alignItems: "center",
-                    marginTop: 32
-                }}>
-                <SettingPicker
-                    title="Display Language"
-                    selectedValue={this.state.displayLanguage}
-                    onValueChange={value => store.dispatch({
-                        type: "CHANGE_DISPLAY_LANGUAGE",
-                        payload: value
-                    })}
-                    items={["English"]}
-                />
-                <SettingPicker
-                    title="Learn Language"
-                    selectedValue={this.state.learnLanguage}
-                    onValueChange={value => store.dispatch({
-                        type: "CHANGE_LEARN_LANGUAGE",
-                        payload: value
-                    })}
-                    items={["日本語"]}
-                />
-                <View
-                    style={{
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        width: "85%",
-                        height: 48,
-                        alignItems: "center",
-                    }}>
-                    <Text style={{
-                        fontSize: 18
-                    }}>Night Mode</Text>
-                    <Switch
-                        value={this.state.nightMode}
-                        onValueChange={() => store.dispatch({ type: "CHANGE_NIGHT_MODE" })} />
-                </View>
-                <View
-                    style={{
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        width: "85%",
-                        height: 48,
-                        alignItems: "center",
-                    }}>
-                    <Text style={{
-                        fontSize: 18
-                    }}>Auto Speak</Text>
-                    <Switch
-                        value={this.state.autoSpeak}
-                        onValueChange={() => store.dispatch({ type: "CHANGE_AUTO_SPEAK" })} />
-                </View>
-                <View
-                    style={{
-                        flexDirection: "row",
-                        justifyContent: "space-between",
-                        width: "85%",
-                        height: 48,
-                        alignItems: "center",
-                    }}>
-                    <Text style={{
-                        fontSize: 18
-                    }}>Auto Next</Text>
-                    <Switch
-                        value={this.state.autoNext}
-                        onValueChange={() => store.dispatch({ type: "CHANGE_AUTO_NEXT" })} />
-                </View>
-                <Pressable
-                    android_ripple={{
-                        color: "rgba(0,0,0,0.2)",
-                        borderless: false,
-                    }}
-                    style={{
-                        height: 48,
-                        width: "100%",
-                        flexDirection: "row",
-                        justifyContent: "center"
-                    }}>
-                    <View style={{
-                        height: 48,
-                        width: "85%",
-                        flexDirection: "row",
-                        alignItems: "center",
-                    }}>
-                        <Text
-                            style={{
-                                fontSize: 18
-                            }}>About</Text>
-                    </View>
-                </Pressable>
-            </View>
-        )
-    }
+const SettingsScreen = ({ navigation }) => (
+    <Stack.Navigator>
+        <Stack.Screen
+            name="SettingsHome"
+            component={screens.home}
+            options={{
+                title: "Settings"
+            }} />
+    </Stack.Navigator>
+)
+
+const screens = {
+    home: () => <SettingsHomeScreen />
 }
 
 export { SettingsScreen }
